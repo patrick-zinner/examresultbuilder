d80b3b1e96a62f06c6e9436d2d7b5744e335c215
Patrick Zinner <pzinner@pop-os.localdomain>
2020-03-08 15:12:26 +0100

    init project

---
+++ src/main/kotlin/examresultsbuilder/Application.kt
@@ -0,0 +1,22 @@
+package examresultsbuilder
+
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "examresultsbuilder",
+            version = "0.0"
+    )
+)
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("examresultsbuilder")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
