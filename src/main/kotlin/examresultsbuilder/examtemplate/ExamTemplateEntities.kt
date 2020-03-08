f4c16222dde5f2a54f96c038e2a7dfbd2a9b3db3
Patrick Zinner <zinnsoldat91@gmail.com>
2020-03-08 15:33:58 +0100

    added exam template entities

---
+++ src/main/kotlin/examresultsbuilder/examtemplate/ExamTemplateEntities.kt
@@ -0,0 +1,23 @@
+package examresultsbuilder.examtemplate
+
+import javax.persistence.*
+
+@Entity
+data class ExamTemplate(@Id var id: Long?,
+                        var name: String,
+                        @OneToMany(mappedBy = "exam") val categories: List<TaskCategory>)
+
+
+@Entity
+data class TaskCategory(@Id @GeneratedValue var id: Long?,
+                        @ManyToOne @JoinColumn(name = "exam_id") var exam: ExamTemplate,
+                        @OneToMany(mappedBy = "category") val tasks: List<Task>,
+                        var name: String) {
+
+}
+
+@Entity
+data class Task(@Id @GeneratedValue var id: Long?,
+                @ManyToOne @JoinColumn(name = "category_id") var category: TaskCategory,
+                var name: String?,
+                var maxScore: Int)
